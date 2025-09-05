f3187782ec55d122f6c68e28423aa6d10042bcb3
jake-t-dev <krysis15@outlook.com>
2025-09-05 14:37:28 +0100

    add timestamps

--- server/src/routes/creatures.ts
+++ server/src/routes/creatures.ts
@@ -11,9 +11,10 @@ creatureRouter.get("/", async (req, res) => {
 
 creatureRouter.post("/", async (req, res) => {
   const { name, image, initiative } = req.body;
+  const createdAt = new Date();
   const creature = await client.query(
-    "INSERT INTO creatures (name, image, initiative) VALUES ($1, $2, $3) RETURNING *",
-    [name, image, initiative]
+    "INSERT INTO creatures (name, image, initiative, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [name, image, initiative, createdAt, createdAt]
   );
   res.json(creature.rows[0] as Creature);
 });
@@ -21,9 +22,10 @@ creatureRouter.post("/", async (req, res) => {
 creatureRouter.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { name, image, initiative } = req.body;
+  const updatedAt = new Date();
   const creature = await client.query(
-    "UPDATE creatures SET name = $1, image = $2, initiative = $3 WHERE id = $4 RETURNING *",
-    [name, image, initiative, id]
+    "UPDATE creatures SET name = $1, image = $2, initiative = $3, updated_at = $4 WHERE id = $5 RETURNING *",
+    [name, image, initiative, updatedAt, id]
   );
   res.json(creature.rows[0] as Creature);
 });
